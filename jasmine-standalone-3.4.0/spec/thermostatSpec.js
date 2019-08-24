0b98607133c790fb56bdfa995fbe18674546dfb1
sumeet978 <searchsumeet@gmail.com>
2019-08-24 12:44:48 +0100

    fully-tested thermostat is ready to go :D

--- jasmine-standalone-3.4.0/spec/thermostatSpec.js
+++ jasmine-standalone-3.4.0/spec/thermostatSpec.js
@@ -61,10 +61,35 @@ describe('Thermostat',function(){
     });
   });
   it('can be reset to the default temperature', function() {
-    for (var i = 0; i < 6; i++) {
+    for (var i = 0; i < 26; i++) {
       thermostat.up();
     }
     thermostat.resetTemperature();
     expect(thermostat.getCurrentTemperature()).toEqual(20);
   });
+  describe('displaying usage levels', function() {
+    describe('when the temperature is below 18 degrees', function() {
+      it('it is considered low-usage', function() {
+        for (var i = 0; i < 3; i++) {
+          thermostat.down();
+        }
+        expect(thermostat.energyUsage()).toEqual('low-usage');
+      });
+    });
+    describe('when the temperature is between 18 and 25 degrees', function() {
+      it('it is considered medium-usage', function() {
+        expect(thermostat.energyUsage()).toEqual('medium-usage');
+      });
+    });
+  
+    describe('when the temperature is anything else', function() {
+      it('it is considered high-usage', function() {
+        thermostat.powerSavingMode = false;
+        for (var i = 0; i < 6; i++) {
+          thermostat.up();
+        }
+        expect(thermostat.energyUsage()).toEqual('high-usage');
+      });
+    });
+});
 });
--- jasmine-standalone-3.4.0/src/thermostat.js
+++ jasmine-standalone-3.4.0/src/thermostat.js
@@ -6,6 +6,7 @@ function Thermostat() {
   this.powerSavingMode = true;
   this.MAX_LIMIT_PSM_ON = 25;
   this.MAX_LIMIT_PSM_OFF = 32;
+  this.MEDIUM_ENERGY_USAGE_LIMIT = 18;
 }
 
   Thermostat.prototype.getCurrentTemperature = function(){
@@ -50,5 +51,14 @@ function Thermostat() {
   };
   Thermostat.prototype.resetTemperature = function() {
     this.temperature = this.DEFAULT_TEMPERATURE;
+  };
+  Thermostat.prototype.energyUsage = function() {
+    if (this.temperature < this.MEDIUM_ENERGY_USAGE_LIMIT) {
+      return 'low-usage';
+    }
+    if (this.temperature >= this.MEDIUM_ENERGY_USAGE_LIMIT && this.temperature <= this.MAX_LIMIT_PSM_ON) {
+      return 'medium-usage';
+    }
+    return 'high-usage';
   };
 
