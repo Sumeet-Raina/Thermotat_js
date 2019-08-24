e77b69c444a89f676ae61dce093825ae8e22a9e5
sumeet978 <searchsumeet@gmail.com>
2019-08-24 11:31:46 +0100

    Thermostat Js added

---
+++ jasmine-standalone-3.4.0/spec/thermostatSpec.js
@@ -0,0 +1,70 @@
+'use strict';
+describe('Thermostat',function(){
+  var thermostat;
+  beforeEach(function(){
+    thermostat = new Thermostat();
+  })
+  it('starts at 20 degrees', function(){
+     expect(thermostat.getCurrentTemperature()).toEqual(20);
+  }); 
+  it('increases in temperature with up()', function(){
+    thermostat.up();
+    expect(thermostat.getCurrentTemperature()).toEqual(21);
+  })
+  it('decreases in temperature with down()', function(){
+    thermostat.down();
+    expect(thermostat.getCurrentTemperature()).toEqual(19);
+  });
+  it('has a minimum temperature of 10 degrees', function(){
+    for(var i = 0 ; i < 11; i ++){
+      thermostat.down();
+    }
+    expect(thermostat.getCurrentTemperature()).toEqual(10);
+  });
+  it('has power saving mode on by default', function() {
+    expect(thermostat.isPowerSavingModeOn()).toBe(true);
+  });
+  it('can switch PSM off', function() {
+    thermostat.switchPowerSavingModeOff();
+    expect(thermostat.isPowerSavingModeOn()).toBe(false);
+  });
+  it('can switch PSM back on',function(){
+    thermostat.switchPowerSavingModeOff();
+    expect(thermostat.isPowerSavingModeOn()).toBe(false);
+    thermostat.switchPowerSavingModeOn();
+    expect(thermostat.isPowerSavingModeOn()).toBe(true);
+  });
+  describe('when power saving mode is on', function() {
+    it('has a maximum temperature of 25 degrees', function() {
+      for (var i = 0; i < 26; i++) {
+        thermostat.up();
+      }
+      expect(thermostat.getCurrentTemperature()).toEqual(25);
+    });
+  });
+  describe('when power saving mode is off', function() {
+    it('has a maximum temperature of 32 degrees', function() {
+      thermostat.switchPowerSavingModeOff();
+      for (var i = 0; i < 13; i++) {
+        thermostat.up();
+      }
+      expect(thermostat.getCurrentTemperature()).toEqual(32);
+    });
+  });
+  describe('when power saving mode is off', function() {
+    it('has a maximum temperature of 32 degrees', function() {
+      thermostat.switchPowerSavingModeOff();
+      for (var i = 0; i < 33; i++) {
+        thermostat.up();
+      }
+      expect(thermostat.getCurrentTemperature()).toEqual(32);
+    });
+  });
+  it('can be reset to the default temperature', function() {
+    for (var i = 0; i < 6; i++) {
+      thermostat.up();
+    }
+    thermostat.resetTemperature();
+    expect(thermostat.getCurrentTemperature()).toEqual(20);
+  });
+});
---
+++ jasmine-standalone-3.4.0/src/thermostat.js
@@ -0,0 +1,54 @@
+'use strict';
+function Thermostat() {
+  this.MINIMUM_TEMPERATURE = 10;
+  this.DEFAULT_TEMPERATURE = 20;
+  this.temperature = this.DEFAULT_TEMPERATURE;
+  this.powerSavingMode = true;
+  this.MAX_LIMIT_PSM_ON = 25;
+  this.MAX_LIMIT_PSM_OFF = 32;
+}
+
+  Thermostat.prototype.getCurrentTemperature = function(){
+     return this.temperature;
+  };
+  Thermostat.prototype.up = function(){
+     this.temperature += 1;
+  };
+  
+  Thermostat.prototype.down = function(){
+    if(this.isMinimumTemperature()){
+      return;
+    }
+    this.temperature -= 1;
+  };
+  Thermostat.prototype.isMinimumTemperature = function() {
+    return this.temperature === this.MINIMUM_TEMPERATURE;
+  };
+  Thermostat.prototype.isMaximumTemperature = function() {
+    return this.temperature === this.MAX_LIMIT_PSM_ON;
+  };
+  Thermostat.prototype.isPowerSavingModeOn = function(){
+    return this.powerSavingMode === true;
+  }
+  Thermostat.prototype.switchPowerSavingModeOff = function(){
+    return this.powerSavingMode = false;
+  };
+  Thermostat.prototype.switchPowerSavingModeOn = function(){
+    return this.powerSavingMode = true;
+  };
+  Thermostat.prototype.up = function() {
+    if (this.isMaximumTemperature()) {
+      return;
+    }
+    this.temperature += 1;
+  };
+  Thermostat.prototype.isMaximumTemperature = function() {
+    if (this.isPowerSavingModeOn() === false) {
+      return this.temperature === this.MAX_LIMIT_PSM_OFF;
+    }
+    return this.temperature === this.MAX_LIMIT_PSM_ON;
+  };
+  Thermostat.prototype.resetTemperature = function() {
+    this.temperature = this.DEFAULT_TEMPERATURE;
+  };
+
